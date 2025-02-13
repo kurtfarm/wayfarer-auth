980791f407928951da186ece6b8107a7003dc5aa
Jhzlo <kjh0010703@naver.com>
2025-02-13 21:34:58 +0900

    chore: jjwt 구현체 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     runtimeOnly("mysql:mysql-connector-java:8.0.33")
 
