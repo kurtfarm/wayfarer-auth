3b1286a1fcc8df81c43178bf08c89ceb9513fe3e
Jhzlo <kjh0010703@naver.com>
2025-02-15 16:40:53 +0900

    feat(token-service): refresh token generate & save

--- src/main/kotlin/wayfarer_auth/jwt/service/TokenService.kt
+++ src/main/kotlin/wayfarer_auth/jwt/service/TokenService.kt
@@ -4,41 +4,50 @@ import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 import org.springframework.stereotype.Service
 import wayfarer_auth.jwt.config.JwtProperties
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 @Service
 class TokenService(
-    private val jwtProperties: JwtProperties
+    private val jwtProperties: JwtProperties,
+    private val redisTemplate: RedisTemplate<String, Any>
 ) {
     private val signKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtProperties.secretKey)) // access token secret key
     fun generateAccessToken(userId: Long): String {
         val now = Date()
         val expiryDate = Date(now.time + jwtProperties.accessTokenExpiration)
 
-        return makeToken(expiryDate, userId)
+        return makeToken(expiryDate, userId, "ACCESS")
     }
 
     fun generateRefreshToken(userId: Long): String {
         val now = Date()
         val expiryDate = Date(now.time + jwtProperties.refreshTokenExpiration)
+        val refreshToken = makeToken(expiryDate, userId, "REFRESH")
 
-        return makeToken(expiryDate, userId)
+        saveRefreshToken(refreshToken, userId)
+
+        return refreshToken
     }
 
-    private fun makeToken(expiry: Date, userId: Long): String {
-        val now = Date()
-
-        return Jwts.builder()
+    private fun makeToken(expiry: Date, userId: Long, tokenType: String): String =
+        Jwts.builder()
             .setSubject(userId.toString())
-            .setIssuedAt(now)
+            .claim("type", tokenType)
+            .setIssuedAt(Date())
             .setExpiration(expiry)
             .signWith(signKey)
             .compact()
+
+    private fun saveRefreshToken(token: String, userId: Long){
+        val key = "refreshToken:$userId" // 사용자별 키 관리
+        redisTemplate.opsForValue().set(key, token, jwtProperties.refreshTokenExpiration, TimeUnit.MILLISECONDS)
     }
 
 //    fun validateAccessToken(token: String): Boolean {
--- src/test/kotlin/wayfarer_auth/config/jwt/TokenServiceTest.kt
+++ src/test/kotlin/wayfarer_auth/config/jwt/TokenServiceTest.kt
@@ -1,77 +1,81 @@
-package wayfarer_auth.config.jwt
+    package wayfarer_auth.config.jwt
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import wayfarer_auth.jwt.config.JwtProperties
-import wayfarer_auth.jwt.service.TokenService
+    import org.assertj.core.api.Assertions.assertThat
+    import org.junit.jupiter.api.BeforeEach
+    import org.junit.jupiter.api.Test
+    import org.springframework.beans.factory.annotation.Autowired
+    import org.springframework.boot.test.context.SpringBootTest
+    import org.springframework.data.redis.core.RedisTemplate
+    import wayfarer_auth.jwt.config.JwtProperties
+    import wayfarer_auth.jwt.service.TokenService
 
-@SpringBootTest
-class TokenServiceTest {
+    @SpringBootTest
+    class TokenServiceTest {
 
-    @Autowired
-    private lateinit var tokenProvider: TokenService
+        @Autowired
+        private lateinit var tokenProvider: TokenService
 
-    @Autowired
-    private lateinit var jwtProperties: JwtProperties
+        @Autowired
+        private lateinit var jwtProperties: JwtProperties
 
-    @BeforeEach
-    fun setUp() {
-        tokenProvider = TokenService(jwtProperties)
+        @Autowired
+        private lateinit var redisTemplate: RedisTemplate<String, Any>
+
+        @BeforeEach
+        fun setUp() {
+            tokenProvider = TokenService(jwtProperties, redisTemplate)
+        }
+
+        @Test
+        fun `유저의 아이디를 전달해 Access Token을 만들 수 있다`() {
+            // Given
+            val userId = 1L
+
+            // When
+            val token = tokenProvider.generateAccessToken(userId)
+
+            // Then
+            assertThat(token).isNotNull()
+            assertThat(token).isNotEmpty()
+            println("Generated Token: $token")
+        }
+
+    //    @Test
+    //    fun `만료된 토큰은 유효성 검증에 실패한다`() {
+    //        // Given
+    //        val expiredToken = generateExpiredToken()
+    //
+    //        // When & Then
+    //        val exception = assertThrows(ExpiredJwtException::class.java) {
+    //            tokenProvider.validateAccessToken(expiredToken)
+    //        }
+    //        assertNotNull(exception)
+    //        println("Expired Token Exception: ${exception.message}")
+    //    }
+
+        @Test
+        fun `Access Token 기반으로 인증 정보를 가져올 수 있다`() {
+            // Given
+            val userId = 1L
+            val token = tokenProvider.generateAccessToken(userId)
+
+            // When
+            val authentication = tokenProvider.getAuthentication(token)
+
+            // Then
+            assertThat(authentication).isNotNull()
+            assertThat(authentication.name).isEqualTo(userId.toString())
+            assertThat(authentication.authorities).isNotEmpty()
+        }
+
+    //    private fun generateExpiredToken(): String {
+    //        val now = Date()
+    //        val expiredDate = Date(now.time - 1000)
+    //        return Jwts.builder()
+    //            .setSubject("expiredUser")
+    //            .setIssuedAt(now)
+    //            .setExpiration(expiredDate)
+    //            .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtProperties.secretKey)))
+    //            .compact()
+    //    }
     }
-
-    @Test
-    fun `유저의 아이디를 전달해 토큰을 만들 수 있다`() {
-        // Given
-        val userId = 1L
-
-        // When
-        val token = tokenProvider.generateToken(userId)
-
-        // Then
-        assertThat(token).isNotNull()
-        assertThat(token).isNotEmpty()
-        println("Generated Token: $token")
-    }
-
-//    @Test
-//    fun `만료된 토큰은 유효성 검증에 실패한다`() {
-//        // Given
-//        val expiredToken = generateExpiredToken()
-//
-//        // When & Then
-//        val exception = assertThrows(ExpiredJwtException::class.java) {
-//            tokenProvider.validateAccessToken(expiredToken)
-//        }
-//        assertNotNull(exception)
-//        println("Expired Token Exception: ${exception.message}")
-//    }
-
-    @Test
-    fun `토큰 기반으로 인증 정보를 가져올 수 있다`() {
-        // Given
-        val userId = 1L
-        val token = tokenProvider.generateToken(userId)
-
-        // When
-        val authentication = tokenProvider.getAuthentication(token)
-
-        // Then
-        assertThat(authentication).isNotNull()
-        assertThat(authentication.name).isEqualTo(userId.toString())
-        assertThat(authentication.authorities).isNotEmpty()
-    }
-
-//    private fun generateExpiredToken(): String {
-//        val now = Date()
-//        val expiredDate = Date(now.time - 1000)
-//        return Jwts.builder()
-//            .setSubject("expiredUser")
-//            .setIssuedAt(now)
-//            .setExpiration(expiredDate)
-//            .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtProperties.secretKey)))
-//            .compact()
-//    }
-}
