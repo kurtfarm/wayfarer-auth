44d616c00c10521238fba3b948f3b61f469980c6
Jhzlo <kjh0010703@naver.com>
2025-02-18 15:46:06 +0900

    chore: Swagger Config Setting

--- src/main/kotlin/wayfarer_auth/user/config/SecurityConfig.kt
+++ src/main/kotlin/wayfarer_auth/user/config/SecurityConfig.kt
@@ -3,11 +3,11 @@ package wayfarer_auth.user.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity
-//import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-//import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.SecurityFilterChain
 //import org.springframework.web.cors.CorsConfiguration
 //import org.springframework.web.cors.CorsConfigurationSource
 //import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -19,24 +19,27 @@ class SecurityConfig(
 //    private val jwtAuthenticationFilter: JwtAuthenticationFilter
 ) {
 
-//    @Bean
-//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-//        return http
-//            .csrf { it.disable() }
-//            .formLogin { it.disable() }
-//            .httpBasic { it.disable() }
-//            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
-//            //.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
-//            .authorizeHttpRequests {
-//                it.requestMatchers(
-//                    "/api/auth/*"
-//                )
-//                    .permitAll()
-//                    .anyRequest()
-//                    .authenticated()
-//            }
-//            .build()
-//    }
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            //.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .authorizeHttpRequests {
+                it.requestMatchers(
+                    "/api/auth/*",
+                    "/v3/api-docs/**",
+                    "/swagger-ui/**",
+                    "/swagger-ui.html"
+                )
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+            }
+            .build()
+    }
 
     @Bean
     fun passwordEncoder(): PasswordEncoder {
